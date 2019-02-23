efb79848cff4effcb847b76ac49fff692373fe13
gaorui <gaorui47@aliyun.com>
2019-02-23 13:53:04 +0800

    上拉加载更改未完成

--- src/javascripts/controllers/jobs/jobsMain-controller.js
+++ src/javascripts/controllers/jobs/jobsMain-controller.js
@@ -1,40 +1,57 @@
 
 
-// const jobsMain = require("../views/jobs-main.html");
-// const { requestList } = require('../models/jobsMain-models');
 const jobsMain = require("../../views/jobs/jobs-main.html");
 const { requestList } = require('../../models/jobs/jobsMain-models');
+const BScroll = require('better-scroll').default;
+let num = 0;
+let mainBool = false;
 
 
-let mainBool=false;
 
-const render = async () => {
-    let resoultList = await requestList();
-    let res = JSON.parse(resoultList);
-    let template = Handlebars.compile(jobsMain);
-    $('#main').append(template({ data: res.data.data }));
-    init();
+const render = () => {
+    getList();
+
+    // console.log(better);
 }
+
+//遮罩点击事件
 function init() {
-    $('.main-item_search-city').on('click',()=>{
-       if(!mainBool){
-        $('.main-citiesSimple').css('height','100%');
-        $('.main-item_hot').css('overflow','hidden');
-        $('.main-item_search-city i').css('backgroundImage','url("/static/images/city-icon-up.png")');   
-       }else{
-        $('.main-citiesSimple').css('height','0');
-        $('.main-item_hot').css('overflow','auto');
-        $('.main-item_search-city i').css(' backgroundImage',' url("/static/images/city-icon.png")');
-       }
-       mainBool=!mainBool;
+    $('.main-item_search-city').on('click', () => {
+        if (!mainBool) {
+            $('.main-citiesSimple').css('height', '100%');
+            $('.main-item_hot').css('overflow', 'hidden');
+            $('.main-item_search-city i').css('backgroundImage', 'url("/static/images/city-icon-up.png")');
+        } else {
+            $('.main-citiesSimple').css('height', '0');
+            $('.main-item_hot').css('overflow', 'auto');
+            $('.main-item_search-city i').css(' backgroundImage', ' url("/static/images/city-icon.png")');
+        }
+        mainBool = !mainBool;
     });
-    $('.main-citiesSimple').on('click',()=>{
-        $('.main-item_hot').css('overflow','auto');
-        $('.main-item_search-city i').css(' backgroundImage',' url("/static/images/city-icon.png")');
-        $('.main-citiesSimple').css('height','0');
-        mainBool=!mainBool;
+    $('.main-citiesSimple').on('click', () => {
+        $('.main-item_hot').css('overflow', 'auto');
+        $('.main-item_search-city i').css(' backgroundImage', ' url("/static/images/city-icon.png")');
+        $('.main-citiesSimple').css('height', '0');
+        mainBool = !mainBool;
     })
 };
+//获取数据
+async function getList() {
+    num++;
+    let better = new BScroll('#main', {
+        pullUpLoad: true
+    });
+    better.on("pullingUp", () => {
+        console.log("ccc")
+    })
+    let resoultList = await requestList(num);
+    let res = JSON.parse(resoultList);
+    let template = Handlebars.compile(jobsMain);
+    $('#main div').append(template({ data: res.data.data }));
+    init();
+}
+
+
 module.exports = {
     render
 }
